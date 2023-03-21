08318092696cc2e60bf2b285c99311be262f5633
Paul Rogerson <paul.rogerson@northcoders.com>
2023-03-21 14:13:19 +0000

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,9 +1,6 @@
 const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
 
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
+// handle using the correct environment variables here
 
 if (!process.env.PGDATABASE) {
   throw new Error('PGDATABASE not set');
